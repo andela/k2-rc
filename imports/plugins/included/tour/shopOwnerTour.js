c31f8280315f2a44b5866ba60c3439945df39061
Merdoth <ucheya97@gmail.com>
2018-02-02 11:49:41 +0100

    feature(onboard-user): - add tour guide for user who is coming onboard for the first time - update packages by adding introjs - style the tour guide to be user friendly - add details for both shop owner and customer
    [Delivers#153812319]

---
+++ imports/plugins/included/tour/shopOwnerTour.js
@@ -0,0 +1,70 @@
+const tour = [
+  {
+    intro: `<h3>Welcome to Reaction Commerce</h3>
+          <hr>
+          <div>I would like to take you on a quick tour of how to use this application</div>`
+  },
+  {
+    element: ".cart-container",
+    intro: `<h3> Shopping Cart </h3>
+          <hr>
+          <div> Your shopping cart contains all the products you selected for purchase. Click on checkout button to proceed to checkout</div>`,
+    position: "bottom"
+  },
+  {
+    element: ".accounts",
+    intro: `<h3> User Account Options</h3>
+          <hr>
+          <div>You can access user account options. You can also register, signin or signout here</div>`
+  },
+  {
+    element: ".currencies",
+    intro: `<h3> Currency Options</h3>
+          <hr>
+          <div> Select your prefered currency for making payment</div>`
+  },
+  {
+    element: ".languages",
+    intro: `<h3> Language Options</h3>
+          <hr>
+          <div> You can select your prefered language by clicking on this button </div>`
+  },
+  {
+    element: ".search",
+    intro: `<h3>Search Products</h3>
+          <hr>
+          <div> Search for your desired products by clicking on this icon</div>`
+  },
+  {
+    element: ".toolbar-vertical",
+    intro: `<h3>Vendor Dashboard</h3>
+            <hr>
+            <div>Here is your dashboard. You can manage your store, attend to orders, choose your preferred payment methods, etc.</div>`
+  },
+  {
+    element: "nav.rui",
+    intro: `<h3> Toolbar</h3>
+          <hr>
+          <div> You can carry out admin operations like adding, editing and viewing products from this section</div>`
+  },
+  {
+    element: ".product-grid",
+    intro: `<h3>Products Gallery</h3>
+          <hr>
+          <div>This is the product gallery. Any product you upload to your store will be seen here.</div>`,
+    position: "top"
+  },
+  {
+    element: ".left",
+    intro: `<h3> Toggle Modes</h3>
+          <hr>
+          <div> After you must have uploaded products, here you can make changes to your products, like changing the image, price, quantity, etc. Toggle between edit and view mode to make and view changes to your shop or products</div>`
+  },
+  {
+    intro: `<h3>End of Tour</h3>
+          <hr>
+          <div>Thanks for joining me on the tour. Now you can start uploading products to your store and start making money</div>`
+  }
+];
+
+export default tour;
