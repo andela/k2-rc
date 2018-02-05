6ebde39b79b3661bbab783fb97fd4111d677154f
Merdoth <ucheya97@gmail.com>
2018-02-05 12:43:42 +0100

    Bug(fix-user-onboarding-tour): - update shop owner and vendor tour for proper onboarding and rightful use of words
    [Delivers#154917575]

--- imports/plugins/included/tour/shopOwnerTour.js
+++ imports/plugins/included/tour/shopOwnerTour.js
@@ -8,62 +8,49 @@ const tour = [
     element: ".product-grid-list",
     intro: `<h3>Products Gallery</h3>
           <hr>
-          <div>This is the product gallery. Any product you upload to your store will be seen here.</div>`,
+          <div>This is the product gallery for Reaction store, where added products are displayed</div>`,
     position: "top"
   },
   {
-    element: ".cart-container",
-    intro: `<h3> Shopping Cart </h3>
-          <hr>
-          <div> Your shopping cart contains all the products you selected for purchase. Click on checkout button to proceed to checkout</div>`,
-    position: "bottom"
-  },
-  {
     element: ".accounts",
-    intro: `<h3> User Account Options</h3>
+    intro: `<h3>Account Options</h3>
           <hr>
-          <div>You can access user account options. You can also register, signin or signout here</div>`
-  },
-  {
-    element: ".currencies",
-    intro: `<h3> Currency Options</h3>
-          <hr>
-          <div> Select your prefered currency for making payment</div>`
+          <div>You can access your account options, such as profile, dashboard, accounts, orders</div>`
   },
   {
     element: ".languages",
-    intro: `<h3> Language Options</h3>
+    intro: `<h3>Language Options</h3>
           <hr>
-          <div> You can select your prefered language by clicking on this button </div>`
+          <div>You can select your prefered language by clicking on this button </div>`
   },
   {
     element: ".search",
     intro: `<h3>Search Products</h3>
           <hr>
-          <div> Search for your desired products by clicking on this icon</div>`
+          <div>Search for added products in product gallery</div>`
   },
   {
     element: ".toolbar-vertical",
-    intro: `<h3>Vendor Dashboard</h3>
+    intro: `<h3>Control Bar</h3>
             <hr>
-            <div>Here is your dashboard. You can manage your store, attend to orders, choose your preferred payment methods, etc.</div>`
+            <div>This is where you control different parts of the application such as store management, viewing and managing orders and more...</div>`
   },
   {
     element: "nav.rui",
-    intro: `<h3> Toolbar</h3>
+    intro: `<h3>Toolbar</h3>
           <hr>
-          <div> You can carry out admin operations like adding, editing and viewing products from this section</div>`
+          <div>This bar contains other controls like adding, archiving and publishing of products.</div>`
   },
   {
     element: ".left",
     intro: `<h3> Toggle Modes</h3>
           <hr>
-          <div> After you must have uploaded products, here you can make changes to your products, like changing the image, price, quantity, etc. Toggle between edit and view mode to make and view changes to your shop or products</div>`
+          <div>You can switch between edit and view mode and also see a dropdown of available stores</div>`
   },
   {
     intro: `<h3>End of Tour</h3>
           <hr>
-          <div>Thanks for joining me on the tour. Now you can start uploading products to your store and start making money</div>`
+          <div>Thanks for joining me on the tour. Now you can start uploading products to your store</div>`
   }
 ];
 
